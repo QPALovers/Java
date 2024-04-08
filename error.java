a4fead04f8b45d41942b94719d0503797a5b0eef
QPALovers <860440182@qq.com>
2024-04-08 23:23:27 +0800

    第二次提交代码

---
+++ error.java
@@ -0,0 +1,21 @@
+public class error {
+    public static void main(String[] args) {
+        /* 1.数组模拟实现Arrays.toString
+        String[] arr = new String[] {"haha","18"};
+        System.out.println(myToString(arr));*/
+
+
+    }
+    /*1.模拟实现Arrays.toString
+    public static String myToString(String[] arr) {
+        String ret = "[";
+        for (int i = 0; i < arr.length; i++) {
+            ret += arr[i];
+            if (i < arr.length-1) {
+                ret += ",";
+            }
+        }
+        ret += "]";
+        return ret;
+    }*/
+}
